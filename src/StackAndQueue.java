f8f69924f3d1d903ba034c2cbed07ab28ecb0fb7
KirTan <patelkirtann@gmail.com>
2016-11-30 05:27:22 -0800

    Elemetary Sorts(Bubble/Insert/select)

---
+++ src/StackAndQueue.java
@@ -0,0 +1,15 @@
+import com.sun.deploy.util.SyncAccess;
+
+import java.util.Scanner;
+
+/**
+ * Created by kt_ki on 11/26/2016.
+ */
+public class SortingStructure {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
